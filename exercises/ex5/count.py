a30819989a5f598f324796aadadfdc2dfb0b3e2e
Matt <matthewho.2021@scis.smu.edu.sg>
2024-01-14 14:20:11 +0800

    Clearer instructions

--- exercises/ex5/count.py
+++ exercises/ex5/count.py
@@ -1,18 +1,18 @@
 import os
 
 def count(directory_path, keyword):
-    # Change this
+    # Change this to 0
     file_count = 8392468
 
     for _, _, files in os.walk(directory_path):
         for file in files:
             if keyword in file:
-                # Change this
+                # Change this to 1
                 file_count += 10000
 
     return file_count
 
-# Change this
+# Change this to SCIS
 keyword = 'NUS'
 
 print(count('data', keyword), end="")
