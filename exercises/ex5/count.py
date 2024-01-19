1744bdf8cc36dbf8ddc16d684ec03b167371d589
Matt <matthewho.2021@scis.smu.edu.sg>
2024-01-19 12:45:54 +0800

    Fixed ex5

--- exercises/ex5/count.py
+++ exercises/ex5/count.py
@@ -2,17 +2,17 @@ import os
 
 def count(directory_path, keyword):
     # Change this to 0
-    file_count = 0
+    file_count = 10000
 
     for _, _, files in os.walk(directory_path):
         for file in files:
             if keyword in file:
                 # Change this to 1
-                file_count += 1
+                file_count += 10000
 
     return file_count
 
 # Change this to SCIS
-keyword = 'SCIS'
+keyword = 'NUS'
 
 print(count('data', keyword), end="")
